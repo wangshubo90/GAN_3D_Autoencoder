53c4a942d22c789d227926e515e053649eb8ca46
wangshubo90 <34329769+wangshubo90@users.noreply.github.com>
2021-05-21 00:31:01 -0500

    sobel 3D filter finished

---
+++ sobel3D.py
@@ -0,0 +1,46 @@
+from sys import implementation
+from numpy.compat.py3k import getexception
+from numpy.core.arrayprint import dtype_is_implied
+from scipy.signal.signaltools import convolve2d
+import tensorflow as tf
+import numpy as np
+from scipy.signal import convolve2d as conv2d
+
+def sobel3Dfilter(input):
+    """
+    Description: this is an implementation of 3D sobel silter for tensorflow
+    reference: 
+    """
+    smooth = np.array((1,2,1))
+    derivative = np.array((1,0,-1))
+
+    hx = smooth.reshape((1,3))
+    hpx = derivative.reshape((1,3))
+
+    hy = smooth.reshape((3,1))
+    hpy = derivative.reshape((3,1))
+
+    hz = smooth
+    hpz = derivative
+
+    Gx = np.zeros((3,3,3))
+    Gy = np.zeros((3,3,3))
+    Gz = np.zeros((3,3,3))
+
+    for i in range(3):
+        Gx[i,:,:] = hz[i] * conv2d(hpx, hy, mode="full") # h'_x = h'(x) :: h(y) :: h(z) | "::" stands for convolution; mode="full" is very important
+        Gy[i,:,:] = hz[i] * conv2d(hx, hpy, mode="full") # h'_y = h'(y) :: h(x) :: h(z) | this is why we cannot use tf.nn.conv3d which only has 
+        Gz[i,:,:] = hpz[i] * conv2d(hx, hy, mode="full") # h'_y = h'(z) :: h(x) :: h(y) | valid and same padding while scipy.signal has no conv3d. 
+
+    Gx_tf = tf.reshape(tf.constant(Gx, dtype=tf.float32),(3,3,3,1,1))
+    Gx_tf = tf.reshape(tf.constant(Gx, dtype=tf.float32),(3,3,3,1,1))
+    Gx_tf = tf.reshape(tf.constant(Gx, dtype=tf.float32),(3,3,3,1,1))
+
+    grad_x = tf.nn.conv3d(input, Gx_tf, strides=(1,)*5, padding="SAME")
+    grad_y = tf.nn.conv3d(input, Gx_tf, strides=(1,)*5, padding="SAME")
+    grad_z = tf.nn.conv3d(input, Gx_tf, strides=(1,)*5, padding="SAME")
+    
+    output = tf.math.sqrt(
+        tf.math.square(grad_x)+tf.math.square(grad_y)+tf.math.square(grad_z))
+
+    return output
