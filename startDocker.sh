1b00a36243db67813c767ecedd71cb476328676c
Shubo Wang <Shubo.Wang@STJUDE.ORG>
2021-05-21 21:04:37 +0000

    ignored .pyc, .vscode/, .ipynb_checkpoints/

--- startDocker.sh
+++ startDocker.sh
@@ -3,8 +3,8 @@
 DOCKER_IMAGE=horovod
 
 DOCKER_Run_Name=uctgan
-WORK_SPACE=$HOME/repos/GAN_3D_Autoencoder
-DATA_DIR=$HOME/data
+WORK_SPACE=/opt/swang14/repos/GAN_3D_Autoencoder
+DATA_DIR=/opt/swang14/Data
 jnotebookPort=8888
 GPU_IDs=all
 
