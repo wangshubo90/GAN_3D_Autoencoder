6d845684d27bf1efe00304f2e9ccf8b8dc186172
wangshubo90 <34329769+wangshubo90@users.noreply.github.com>
2021-08-25 05:24:58 +0000

    tuning

--- gan_infer.py
+++ gan_infer.py
@@ -4,7 +4,7 @@ import numpy as np
 from model.AAE import AAE   
 import matplotlib.pyplot as plt
 import SimpleITK as sitk 
-
+from sklearn.model_selection import train_test_split
 checkpoint_dir = "/uctgan/data/ray_results/AAE_uct_test2/AAETrainable_16ccf_00002_2_optD_lr=3.3526e-06,optG_lr=1.6763e-05_2021-08-25_00-47-57"
 checkpoint = "checkpoint_002479"
 param = "params.json"
@@ -19,10 +19,24 @@ model.autoencoder.load_weights("/uctgan/data/ray_results/AAE_uct_test/AAETrainab
 # latest = tf.train.latest_checkpoint(checkpoint_dir)
 # checkpoint_restore.restore(latest).assert_consumed()
 
+def read_data(file_ls):
+    dataset = np.zeros(shape=[len(file_ls), 48, 96, 96, 1], dtype=np.float32)
+    
+    for idx, file in enumerate(file_ls):
+        img = sitk.ReadImage(file)
+        img = sitk.GetArrayFromImage(img)
+        img = img[:,2:98,2:98,np.newaxis].astype(np.float32) / 255.
+        dataset[idx] = img
+    return dataset
+
 datapath = r'/uctgan/data/udelCT'
 file_reference = r'./data/udelCT/File_reference.csv'
+seed=42
 img_ls = glob.glob(os.path.join(datapath, "*.nii*"))
-image = sitk.GetArrayFromImage(sitk.ReadImage(img_ls[-12]))
+train_img, test_img = train_test_split(img_ls, test_size=0.3, random_state=seed)
+val_img, evl_img = train_test_split(test_img, test_size=0.5, random_state=seed)
+
+image = sitk.GetArrayFromImage(sitk.ReadImage(val_img[-9]))
 image= image[:, 2:98, 2:98].reshape((1,48,96,96,1))
 gen_image=np.squeeze(model.autoencoder.predict(image.astype(np.float32)/255)) * 255
 image = np.squeeze(image)
