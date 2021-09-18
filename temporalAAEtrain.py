bc050094278145b882df86a91506873341ceae85
wangshubo90 <34329769+wangshubo90@users.noreply.github.com>
2021-09-18 02:21:50 -0500

    temporalAAE v2

--- temporalAAEtrain.py
+++ temporalAAEtrain.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 import SimpleITK as sitk 
 import numpy as np
-from model.temporaAAE import temporalAAE
+from model.temporalAAEv2 import temporalAAEv2
 
 from tqdm import tqdm
 from utils.losses import *
@@ -30,7 +30,7 @@ config = pickle.load(open(os.path.join(logdir, "config.pkl"), "rb"))
 model_checkpoints = glob.glob(os.path.join(logdir, "*.h5"))
 best_model = natsorted(model_checkpoints)[-1]
 
-tAAE = temporalAAE(
+tAAE = temporalAAEv2(
     AAE_config=config, 
     AAE_checkpoint=best_model,
     lstm_layers=[32,32]
@@ -39,25 +39,28 @@ tAAE = temporalAAE(
 fileRef = r"../training/seq_file_ref.csv"
 dataSrcDir = r"../data"
 
-def readDatasetGenerator(file, dataSrcDir, subset="train", batch_size=3, randseed=42):
+def readDatasetGenerator(file, dataSrcDir, subset="train", batch_size=3, randseed=42, seq_length=4):
     """
     subset : choose from "train", "validate", "evaluate"
     """
     df = pd.read_csv(file)
     df = df[df["subset"]==subset]
-    df = {i:df[df["seq_length"]==i].reset_index() for i in range(2,6)}
+    # df = {i:df[df["seq_length"]==i].reset_index() for i in range(2,6)}
     random.seed(randseed)
     def generator():
         sidx=0
         while True:
-            length = random.randint(2,5)
-            subdf = df[length].sample(n=batch_size, random_state=randseed+sidx)
+            # length = random.randint(2,5)
+            # subdf = df[length].sample(n=batch_size, random_state=randseed+sidx)
+            subdf = df.sample(n=batch_size, random_state=randseed+sidx)
             file_seq_list =  subdf.file_names.apply(lambda x: [i.strip("'") for i in x.strip("[]").split(", ")]).to_list()
             target_list = subdf.tar_img.to_list()
             
-            xdata = np.zeros(shape=(len(file_seq_list), length, 48, 96, 96, 1), dtype=np.float32)
+            xdata = np.zeros(shape=(len(file_seq_list), seq_length, 48, 96, 96, 1), dtype=np.float32)
             ydata = np.zeros(shape=(len(file_seq_list), 48, 96, 96, 1), dtype=np.float32)
             for idx, (seq, tar) in enumerate(zip(file_seq_list, target_list)):
+                if len(seq) > 4:
+                    seq = seq[-4:] 
                 for step, file in enumerate(seq):
                     img = sitk.ReadImage(os.path.join(dataSrcDir, file))
                     img = sitk.GetArrayFromImage(img)
@@ -72,12 +75,17 @@ def readDatasetGenerator(file, dataSrcDir, subset="train", batch_size=3, randsee
             yield xdata, ydata
     return generator()
 
-traingen = readDatasetGenerator(fileRef, dataSrcDir, "train")
-valgen = readDatasetGenerator(fileRef, dataSrcDir, "validate")
+train_set = readDatasetGenerator(fileRef, dataSrcDir, "train")
+val_set = readDatasetGenerator(fileRef, dataSrcDir, "validate")
+logdir=r"../Gan_log/temporalAAE-first-run"
+summary = tAAE.train(train_set, val_set, 10, logdir=logdir, logstart=1, logimage=3, slices=[slice(None), 36])
+# x, y = next(traingen)
+# val_x, val_y = next(valgen)
 
-x, y = next(traingen)
-val_x, val_y = next(valgen)
-_=tAAE.__train_step__(x, y, val_x, val_y, 0.01)
+# a = np.zeros(shape=(3,4,48,96,96,1))
+# b = np.zeros((3, 48,96,96,1), np.float32)
+# _=tAAE.__train_step__(a, b, a, b, 0.01)
+# _=tAAE.__train_step__(x, y, val_x, val_y, 0.01)
 
 
 
