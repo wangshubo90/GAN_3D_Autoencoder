94155aaba38a90dbe5a849e951987d29863d6382
wangshubo90 <34329769+wangshubo90@users.noreply.github.com>
2021-10-08 00:35:29 -0500

    mrs model

--- test_VAE3Dreg.py
+++ test_VAE3Dreg.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
-from model.VAE3Dseg import VAE3Dseg
+from model.VAEmrs import VAEmrs, build_model
 import numpy as np
 import os
 
-model = VAE3Dseg((16,128,128,4),5)
+shape = (60,60,15,6)
+model = VAEmrs(shape, 6)
 
-shape = (16,128,128,4)
+
 x = np.zeros(shape=(1, *shape), dtype=np.float32)
-y = np.ones(shape=(1, 16,128,128,5), dtype=np.float32)
+y = np.ones(shape=(1, 6,6,8), dtype=np.float32)
 
-r = model.train_step(1,1)
+r = model.train_step(model.fetch_batch(1,1))
+r[0]
